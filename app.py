78bc9f15fa02aa029b735d124f685ae73f32424d
EC2 Default User <ec2-user@ip-172-31-38-161.ap-southeast-1.compute.internal>
2023-07-04 02:01:25 +0000

    first commit

--- app.py
+++ app.py
@@ -1,43 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, flash, session
 import boto3
 import key_config as keys
 import dynamodb_handler
 import json
 from boto3.dynamodb.conditions import Key, Attr
+import urllib.parse
 
 app = Flask(__name__)
+app.secret_key = 'flask_server'
 
 dynamodb = boto3.resource(
     'dynamodb',
-    #aws_access_key_id     = keys.ACCESS_KEY_ID,
-    #aws_secret_access_key = keys.ACCESS_SECRET_KEY,
+    aws_access_key_id     = keys.ACCESS_KEY_ID,
+    aws_secret_access_key = keys.ACCESS_SECRET_KEY,
     region_name = keys.REGION_NAME,
 )
 
 @app.route('/')
 def index():
+    
+    #create table below
     #dynamodb_handler.create_user_table()
     #return 'Table Created'
-    return render_template('sign-in.html')
+    
+    #create bucket below to upload image
+    #dynamodb_handler.create_bucket()
+    #return "bucket created"
+    
+    return render_template('sign-up.html')
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    messages = session.pop('flash_messages', [])
+    return render_template('login.html', messages=messages)
     
 
 @app.route('/redirect')
-def redirect():
-    return render_template("sign-in.html")
+def redirect_to_sign_in():
+    return redirect(url_for('index'))
     
     
 @app.route('/signup', methods=['post'])
 def signup():
     
     user_data = request.form.to_dict()
-    
     dynamodb_handler.add_item_to_user_table(user_data['fullname'], int(user_data['registration_number']), user_data['email'], user_data['password'], user_data['degree'], (user_data['contact']), user_data['introduction'], user_data['gpa'], user_data['skills'])
+    flash ("Registration completed successfully!")
 
-    return  render_template("login.html")
+    return redirect(url_for('login'))
 
 
 @app.route('/check', methods=['post'])
@@ -47,45 +57,84 @@ def check():
     
     table = dynamodb.Table('users')
     
+    
     response = table.query(
         KeyConditionExpression=Key("email").eq(email)
     )
     
-    items = response['Items']
-    
-    fullname = items[0]['fullname']
-    registration_number = items[0]['registration_number']
-    password = items[0]['password']
-    degree = items[0]['degree']
-    contact = items[0]['contact']
-    introduction = items[0]['introduction']
-    gpa = items[0]['GPA']
-    skills = items[0]['skills']
-    
+    if response['Items']:
+        items = response['Items']
+        fullname = items[0]['fullname']
+        registration_number = items[0]['registration_number']
+        password = items[0]['password']
+        degree = items[0]['degree']
+        contact = items[0]['contact']
+        introduction = items[0]['introduction']
+        gpa = items[0]['GPA']
+        skills = items[0]['skills']
+        profile_image = items[0]['profile_image']
+    else:
+        flash("User not found try again!")
+        return redirect(url_for("login"))
     
     if login_password == password:
         return render_template('update.html', email=email, password=password, fullname=fullname, registration_number=registration_number, 
-        degree=degree, contact=contact, introduction=introduction, gpa=gpa, skills=skills)
+        degree=degree, contact=contact, introduction=introduction, gpa=gpa, skills=skills, profile_image=profile_image)
+    else:
+        flash ("Invalid password try again!")
+        return redirect(url_for("login"))
         
-        
+
+@app.route('/upload', methods=['POST', 'PUT'])
+def upload_user_profile_image():   
+    
+    filename, bucket_name = dynamodb_handler.upload_image()
+    
+    encoded_object_key = urllib.parse.quote(filename)
+    object_url = f"https://{bucket_name}.s3.amazonaws.com/{encoded_object_key}"
+    
+    dynamodb_handler.update_image_url(object_url)
+    
+    email = request.form['email']
+    
+    profile = dynamodb_handler.get_user_details(email)
+    fullname = profile["fullname"]
+    email = profile["email"]
+    registration_number = profile["registration_number"]
+    password = profile["password"]
+    degree = profile["degree"]
+    contact = profile["contact"]
+    introduction = profile["introduction"]
+    gpa = profile["GPA"]
+    skills = profile["skills"]
+    profile_image = profile["profile_image"]
+    
+    return render_template('update.html', email=email, password=password, fullname=fullname, registration_number=registration_number, 
+    degree=degree, contact=contact, introduction=introduction, gpa=gpa, skills=skills, profile_image=profile_image)
+
+
+    
 @app.route('/update/<string:email>', methods=['PUT'])
 def update_profile(email):
     data = request.get_json()
     
-    response = dynamodb_handler.update_user_profile(email,data)
+    dynamodb_handler.update_user_profile(email,data)
     
-    if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
-        return {
-            'msg'                : 'Updated successfully',
-            'ModifiedAttributes' : response['Attributes'],
-            'response'           : response['ResponseMetadata']
-        }
+    profile = dynamodb_handler.get_user_details(email)
+    fullname = profile["fullname"]
+    email = profile["email"]
+    registration_number = profile["registration_number"]
+    password = profile["password"]
+    degree = profile["degree"]
+    contact = profile["contact"]
+    introduction = profile["introduction"]
+    gpa = profile["GPA"]
+    skills = profile["skills"]
+    profile_image = profile["profile_image"] 
+        
+    return render_template('update.html', email=email, password=password, fullname=fullname, registration_number=registration_number, 
+    degree=degree, contact=contact, introduction=introduction, gpa=gpa, skills=skills, profile_image=profile_image)
 
-    return {
-        'msg'      : 'Some error occured',
-        'response' : response
-    }
-    
     
 @app.route('/view/<int:registration_number>', methods=['GET'])
 def public_profile_view(registration_number):
@@ -99,8 +148,9 @@ def public_profile_view(registration_number):
     introduction = profile["introduction"]
     GPA = profile["GPA"]
     skills = profile["skills"]
+    profile_image = profile["profile_image"]
         
-    return render_template("view.html", fullname=fullname, email=email, registration_number=registration_number, degree=degree, contact=contact, introduction=introduction, GPA=GPA, skills=skills)
+    return render_template("view.html", fullname=fullname, email=email, registration_number=registration_number, degree=degree, contact=contact, introduction=introduction, GPA=GPA, skills=skills, profile_image=profile_image)
 
         
 
--- dynamodb_handler.py
+++ dynamodb_handler.py
@@ -1,5 +1,5 @@
 import boto3
-
+from flask import request
 import key_config as keys
 
 
@@ -15,35 +15,53 @@ dynamodb_resource = boto3.resource(
     aws_secret_access_key = keys.ACCESS_SECRET_KEY,
     region_name           = keys.REGION_NAME,
 )
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id     = keys.ACCESS_KEY_ID,
+    aws_secret_access_key = keys.ACCESS_SECRET_KEY,
+    region_name           = keys.REGION_NAME,
+)
+
 
 def create_user_table():
     
-   table = dynamodb_resource.create_table(
-       TableName = 'users', # Name of the table
-       KeySchema = [
-           {
+    table = dynamodb_resource.create_table(
+        TableName = 'users', # Name of the table
+        KeySchema = [
+            {
                'AttributeName': 'email',
                'KeyType'      : 'HASH' #RANGE = sort key, HASH = partition key
-           }
-       ],
-       AttributeDefinitions = [
+            }
+        ],
+        AttributeDefinitions = [
            {
                'AttributeName': 'email', # Name of the attribute
                'AttributeType': 'S'   # N = Number (B= Binary, S = String)
            }
-       ],
-       ProvisionedThroughput={
+        ],
+        ProvisionedThroughput={
            'ReadCapacityUnits'  : 10,
            'WriteCapacityUnits': 10
-       }
-   )
-   return table
+        }
+    )
+    return table
+
+
+def create_bucket():
+    
+    AWS_REGION = "ap-southeast-1"
+    client = boto3.client("s3", region_name=AWS_REGION)
+    bucket_name = "profile-image-upload"
+    location = {'LocationConstraint': AWS_REGION}
+    client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
 
 
 user = dynamodb_resource.Table("users")
 
      
 def add_item_to_user_table(fullname, registration_number, email, password, degree, contact, introduction, GPA, skills):
+    
+    profile_image = "https://profile-image-upload.s3.ap-southeast-1.amazonaws.com/default.png"
     
     response = user.put_item(
         Item={
@@ -55,9 +73,49 @@ def add_item_to_user_table(fullname, registration_number, email, password, degre
              'contact' : contact,
              'introduction' : introduction,
              'GPA' : GPA,
-             'skills' : skills
+             'skills' : skills,
+             'profile_image' : profile_image
         }
     )
+    return response
+    
+    
+def upload_image():
+    
+    file = request.files['profile_image']
+    filename = file.filename
+    bucket_name = 'profile-image-upload'
+    bucket = s3.Bucket(bucket_name)
+    bucket.put_object(
+        Key=filename,
+        Body=file,
+        ContentType='image/jpeg',
+        ContentDisposition='inline'
+    ) 
+    return filename, bucket_name
+   
+   
+def update_image_url(object_url):
+    
+    email = request.form['email']
+    
+    response = user.update_item(
+        
+        Key = {
+           'email': email
+        },
+        AttributeUpdates={
+            
+            'profile_image': {
+               'Value'  : object_url,
+               'Action' : 'PUT' 
+            }
+            
+        },
+        
+        ReturnValues = "UPDATED_NEW"  # returns the new updated values
+    )
+    
     return response
     
     
@@ -109,6 +167,16 @@ def update_user_profile(email,data:dict):
     )
     
     return response
+    
+    
+def get_user_details(email):
+    response = user.get_item(
+        Key={
+            'email': email
+        })
+    item = response["Item"]
+    
+    return item
     
     
 def get_user_profile(registration_number):
